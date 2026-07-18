38939ac408fc158ef2a389177036fe59da53732d
kszuba1 <krzysiu.sz120@gmail.com>
2026-07-18 19:23:58 +0200

    refactor(benchmarks): source postgres image from shared fixture

--- benchmarks/src/test/java/io/github/kszuba1/jooq_hibernate_comparison/benchmarks/TestcontainersConfiguration.java
+++ benchmarks/src/test/java/io/github/kszuba1/jooq_hibernate_comparison/benchmarks/TestcontainersConfiguration.java
@@ -1,10 +1,6 @@
 package io.github.kszuba1.jooq_hibernate_comparison.benchmarks;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UncheckedIOException;
-import java.util.Properties;
-
+import io.github.kszuba1.jooq_hibernate_comparison.core.contract.PostgresImage;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -17,17 +13,7 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse(postgresImage()));
-	}
-
-	static String postgresImage() {
-		Properties properties = new Properties();
-		try (InputStream in = TestcontainersConfiguration.class.getResourceAsStream("/container.properties")) {
-			properties.load(in);
-		} catch (IOException e) {
-			throw new UncheckedIOException("container.properties missing from test classpath", e);
-		}
-		return properties.getProperty("postgres.image");
+		return new PostgreSQLContainer(DockerImageName.parse(PostgresImage.get()));
 	}
 
 }
