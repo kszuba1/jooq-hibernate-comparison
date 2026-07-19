6a322e7cbfa23e60f0c2b762a64aaabc300b153d
kszuba1 <krzysiu.sz120@gmail.com>
2026-07-19 22:31:42 +0200

    feat(persistence-jooq): add tuned-config bootstrap overload

--- persistence-jooq/src/main/java/io/github/kszuba1/jooq_hibernate_comparison/persistence/jooq/JooqContextFactory.java
+++ persistence-jooq/src/main/java/io/github/kszuba1/jooq_hibernate_comparison/persistence/jooq/JooqContextFactory.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
+import org.jooq.conf.Settings;
 import org.jooq.impl.DSL;
 
 public final class JooqContextFactory {
@@ -13,6 +14,10 @@ public final class JooqContextFactory {
 
 	public static DSLContext createContext(DataSource dataSource) {
 		return DSL.using(dataSource, SQLDialect.POSTGRES);
+	}
+
+	public static DSLContext createContext(DataSource dataSource, Settings settings) {
+		return DSL.using(dataSource, SQLDialect.POSTGRES, settings);
 	}
 
 }
